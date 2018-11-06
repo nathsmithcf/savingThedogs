7579fc9e7e2bbc6e6c8c052bc4719e59e4f819cd
Nate <nath_15@hotmail.com>
2018-11-06 06:19:38 -0500

    Initial Commit

---
+++ SavingTheDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SavingTheDogs
+//
+//  Created by Nathaniel Smith on 2018-11-06.
+//  Copyright © 2018 Nathaniel Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
